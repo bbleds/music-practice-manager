dc4471f2036084fddf10e9766f4190226a96bdea
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-11 10:04:07 -0600

    setup serving static files

--- server.js
+++ server.js
@@ -3,9 +3,19 @@
 // ------------- dependencies
 const express = require("express");
 const app = express();
+const path =  require("path");
 
 // -------------  envrionemt variables
 const PORT = process.env.PORT || 3000;
+
+
+//use public directory for static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// index route
+app.get("/", (req, res) =>{
+  res.sendFile('public/html/index.html' , { root : __dirname});
+});
 
 
 // spin up app
