1660ddaf3a12e1a02b724af2af50dbc11960f781
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-17 15:46:15 -0500

    Now filtering posting organization abbreviations

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const knex = require("../knexConfig.js");
+const _ = require("lodash");
+
 
 const exportObject = {};
 
@@ -11,16 +13,23 @@ exportObject.getUserData = (req, res) => {
 };
 
 exportObject.addOrganization = (req, res) => {
-  console.log(req.body);
-  // add new organization into db with name entered from frontend
-  // add logged in users id to organization in db
-  console.log("userrrr");
-  knex("organizations").insert({"name": `${req.body.orgName}`, "user_id": `${req.session.passport.user.userId}`, "orgabbrev": `${req.body.orgAbrev}`})
+  // get data from db, if organization abbreviation already exists, send error message to user
+  knex.select("orgabbrev ").table("organizations")
   .then((data) => {
     console.log(data);
-  })
-  .catch((err)=>{
-    console.log(err);
+    if(_.filter(data, {"orgabbrev": `${req.body.orgAbrev}`}).length < 1){
+      knex("organizations").insert({"name": `${req.body.orgName}`, "user_id": `${req.session.passport.user.userId}`, "orgabbrev": `${req.body.orgAbrev}`})
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+    } else {
+      console.log("nope");
+      res.send("This already exists")
+    }
+
   });
 
 };
